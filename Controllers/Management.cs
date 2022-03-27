7760437ab5920a58fa5468eea3705ba7a5090f56
BorjaDeveloper <borjaerss@gmail.com>
2022-03-27 12:26:07 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/Management.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoTFG.Controllers
+{
+    public class Management : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
