56a6634886876dfe8df33804166c011a8983e4a6
BorjaDeveloper <borjaerss@gmail.com>
2022-04-30 21:52:13 +0200

    Trabajando en las reservas.

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -1,14 +1,36 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoTFG.Model;
 
 namespace ProyectoTFG.Controllers
 {
     public class ClientController : Controller
     {
+        private readonly AnimalShelterDbContext _context;
+
+        public ClientController(AnimalShelterDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Booking(int animalId, DateTime date)
+        {
+            var booking = new Booking
+            {
+                AnimalId = animalId,
+                ClientId = AnimalShelterDbContext.CurrentUser!.Id,
+                Date = date
+            };
+
+            _context.Add(booking);
+
+            _context.SaveChanges();
+
+            return View("BookingDone");
         }
     }
 }
---
+++ Model/Booking.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoTFG.Model
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int ClientId { get; set; }
+        public User Client { get; set; }
+        public int AnimalId { get; set; }
+        public Animal Animal { get; set; }
+
+    }
+}
