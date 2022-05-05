9ba5cd015945ad0f449478d68097f1d519d4f05c
BorjaDeveloper <borjaerss@gmail.com>
2022-05-05 12:53:06 +0200

    Cambio de nombre controlador Management, solucionado el error ya que no disponia de un model para mostrar en la vista. Cambio en diseño de página de animales y voluntarios.

---
+++ Controllers/ManagementController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoTFG.Model;
+using ProyectoTFG.Models;
+
+namespace ProyectoTFG.Controllers
+{
+    public class ManagementController : Controller
+    {
+        private readonly AnimalShelterDbContext _context;
+
+        const int ColumnNumber = 3;
+
+        public ManagementController(AnimalShelterDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            VolunteersModel model = BuildVolunteersModel();
+
+            return View(model);
+        }
+
+        private VolunteersModel BuildVolunteersModel()
+        {
+            var model = new VolunteersModel
+            {
+                Mañana = BuildVolunteerTable(VolunteerTypeEnum.Mañana),
+                Tarde = BuildVolunteerTable(VolunteerTypeEnum.Tarde)
+            };
+
+            return model;
+        }
+
+        private List<List<Volunteer>> BuildVolunteerTable(VolunteerTypeEnum volunteerType)
+        {
+            var result = new List<List<Volunteer>>();
+            var current = new List<Volunteer>();
+
+            foreach (var volunteer in _context.Volunteers.Where(a => a.Type == volunteerType))
+            {
+                if (current.Count >= ColumnNumber)
+                {
+                    result.Add(current);
+                    current = new List<Volunteer>();
+                }
+
+                current.Add(volunteer);
+            }
+
+            result.Add(current);
+
+            return result;
+        }
+    }
+}
